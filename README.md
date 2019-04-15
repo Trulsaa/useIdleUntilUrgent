b11159f04b3ca6756404cb09d92b906a07db9dab
Truls Aagaard <truls.aa@gmail.com>
2019-04-15 10:43:54 +0200

    Init commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # useIdleUntilUrgent
+
 React hook that enables easy use of the idle until urgent component loading strategy
+
+# Usage
+
+```javascript
+const Component = props => {
+  const { open } = props;
+  const FilterTabs = useIdleUntilUrgent(
+    () => import("./IdleUntilUrgentlyLoadedCompoent"),
+    <div>Loading...</div>,
+    open
+  );
+
+  return <IdleUntilUrgentlyLoadedCompoent />;
+};
+```
---
+++ useIdleUntilUrgent.js
@@ -0,0 +1,53 @@
+const idleish = (fn, timeoutFallbackMs = 10000) => {
+  if ("requestIdleCallback" in window) {
+    const handle = requestIdleCallback(fn);
+    return () => cancelIdleCallback(handle);
+  } else {
+    const handle = setTimeout(fn, timeoutFallbackMs);
+    return () => clearTimeout(handle);
+  }
+};
+
+const makeIdleGetter = workFn => {
+  const UNLOADED = {};
+  let result = UNLOADED;
+
+  const clear = idleish(() => {
+    result = workFn();
+  });
+
+  return async () => {
+    if (result === UNLOADED) {
+      result = workFn();
+      clear();
+    }
+
+    return result;
+  };
+};
+
+const useIdleUntilUrgent = (func, fallback, getNow) => {
+  const [{ idleGetter }, setIdleGetter] = useState({
+    idleGetter: () => ({})
+  });
+  const [result, setComponent] = useState();
+
+  const workFn = async () => {
+    const result = await func();
+    setComponent({ component: result.default });
+  };
+
+  useEffect(() => {
+    setIdleGetter({ idleGetter: makeIdleGetter(workFn) });
+  }, []);
+
+  if (getNow && !result) {
+    idleGetter();
+  }
+
+  if (!!result) {
+    return result.component;
+  } else {
+    return () => <div>{fallback}</div>;
+  }
+};
